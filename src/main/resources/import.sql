295d3a443177f508beadd4609d242c1b7c147db7
MNGR <MNGR@IE67LT4N73N62>
2017-08-19 14:54:04 +0530

    import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,21 @@
-insert into  PRODUCT(NAME , PRICE, WEIGHT) values ('Pen',10.10 , 12.00)
+insert into  USER(USER_ID,username , password, city,state) values (1,'samaya','samaya123','bhubaneswar','odisha');
+insert into  USER(USER_ID,username , password, city,state) values (2,'manas','manas123','guhwathi','assam');
+insert into  USER(USER_ID,username , password, city,state) values (3,'tejas','tejas123','bikanar','rajasthan');
+insert into  USER(USER_ID,username , password, city,state) values (4,'jaydeep','jaydeep123','pune','maharastra');
+insert into  USER(USER_ID,username , password, city,state) values (5,'viraj','viraj123','hyderabad','telengana');
+insert into  USER(USER_ID,username , password, city,state) values (6,'vinay','vinay123','bangalore','karnataka');
+insert into  USER(USER_ID,username , password, city,state) values (7,'raju','raju123','viskapatnam','andhrapardesh');
+insert into  USER(USER_ID,username , password, city,state) values (8,'shalini','shalini123','kochi','kerala');
+insert into  USER(USER_ID,username , password, city,state) values (9,'senthil','senthil123','comibataore','tamilnadu');
+insert into  USER(USER_ID,username , password, city,state) values (10,'sourav','sourav123','kolkata','westbengal');
+
+
+
+insert into  INTEREST(INTEREST_ID,DESCRIPTION,name) values (10,'latest movie','movie')
+insert into  INTEREST(INTEREST_ID,DESCRIPTION,name) values (9,'cricket','sport')
+insert into  INTEREST(INTEREST_ID,DESCRIPTION,name) values (8,'bigdata','meetup')
+
+
+insert into  USER_INTEREST(USER_ID,INTEREST_ID) values (10,10)
+insert into  USER_INTEREST(USER_ID,INTEREST_ID) values (9,9)
+insert into  USER_INTEREST(USER_ID,INTEREST_ID) values (8,8)
