dc1c769aa2a7a8de1655403ae6d769807a47e861
MNGR <MNGR@IE67LT530DG32>
2017-08-19 12:30:13 +0530

    update

--- src/main/java/manas/springboot/sampe/myapp/beans/User.java
+++ src/main/java/manas/springboot/sampe/myapp/beans/User.java
@@ -4,26 +4,28 @@ import java.io.Serializable;
 import java.util.Set;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="User")
 public class User implements Serializable {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
-	@Column
-	private String name;
+	@Column(name="USERNAME")
+	private String username;
 
-	@Column
+	@Column(name="CITY")
 	private String city;
 
-	@Column
+	@Column(name="STATE")
 	private String state;
-
-	@Column
-	private Set<Interest> set;
 
 	public long getId() {
 		return id;
@@ -33,12 +35,12 @@ public class User implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getCity() {
@@ -58,54 +60,13 @@ public class User implements Serializable {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((set == null) ? 0 : set.hashCode());
-		result = prime * result + ((state == null) ? 0 : state.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (city == null) {
-			if (other.city != null)
-				return false;
-		} else if (!city.equals(other.city))
-			return false;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (set == null) {
-			if (other.set != null)
-				return false;
-		} else if (!set.equals(other.set))
-			return false;
-		if (state == null) {
-			if (other.state != null)
-				return false;
-		} else if (!state.equals(other.state))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", city=" + city + ", state=" + state + ", set=" + set + "]";
+		return "User [id=" + id + ", username=" + username + ", city=" + city + ", state=" + state + "]";
 	}
+
+	/*@Column
+	private Set<Interest> set;*/
+
+	
 
 }
